4c087c314e41b550962f1799d50276dd5211b0c3
Kaditya67 <322adityakumar0004@dbit.in>
2024-06-13 22:38:50 +0530

    Completed...

--- 48_rotate_image.cpp
+++ 48_rotate_image.cpp
@@ -11,9 +11,13 @@ public:
         int col=matrix[0].size();
        
         for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+            for(int j=0;j<i;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
+        }
+
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 };
